c3ea57ed075a8a77ba997282542a525dc530d5cc
Klim Klimentov <klimentov@i.ua>
2021-01-02 21:51:58 +0200

    create component Header

---
+++ src/header/header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './header.css'
+
+export default class Header extends Component{
+    render() {
+        return <nav className="navbar navBar navbar-expand-lg navbar-dark float-left">
+            <a className="navbar-brand" href="#">Navbar</a>
+                <div className="d-flex navLink navbar-nav justify-content-between">
+                        <a className="nav-link" href="#">Planet</a>
+                        <a className="nav-link" href="#">People</a>
+                        <a className="nav-link " href="#">Starships</a>
+
+                </div>
+
+        </nav>
+    }
+
+}
