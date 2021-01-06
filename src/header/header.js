c6cc5cbc1791b9af85c28a9048be3a95594717d2
Klim Klimentov <klimentov@i.ua>
2021-01-06 23:15:34 +0200

    removed some bugs

--- src/header/header.js
+++ src/header/header.js
@@ -4,11 +4,11 @@ import './header.css'
 export default class Header extends Component{
     render() {
         return <nav className="navbar navBar navbar-expand-lg navbar-dark ">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="/#">Navbar</a>
                 <div className="d-flex navLink navbar-nav justify-content-between">
-                        <a className="nav-link" href="#">Planet</a>
-                        <a className="nav-link" href="#">People</a>
-                        <a className="nav-link " href="#">Starships</a>
+                        <a className="nav-link" href="/#">Planet</a>
+                        <a className="nav-link" href="/#">People</a>
+                        <a className="nav-link " href="/#">Starships</a>
 
                 </div>
 
