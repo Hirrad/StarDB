85e9f2a28426584b92e762e10ae489e68998dbee
Klim Klimentov <klimentov@i.ua>
2021-01-06 23:16:34 +0200

    create serviсe api

--- src/services/swapi.js
+++ src/services/swapi.js
@@ -8,7 +8,31 @@ class SwapiServise {
         }
         return await res.json()
     }
+
+    async getPeople() {
+        const res = await this.getResource('/people/')
+        return res.results
+    }
+    async getPerson(id) {
+        const res = await this.getResource(`/people/${id}`)
+        return res
+    }
+    async getPlanets() {
+        const res = await this.getResource('/planets/')
+        return res.results
+    }
+    async getPlanet(id) {
+        const res = await this.getResource(`/planets/${id}/`)
+        return res
+    }
+
+    async getStarships() {
+        const res = await this.getResource(`/planets/`)
+        return res.results
+    }
+    async getStarship(id) {
+        const res = await this.getResource(`/planets/${id}/`)
+        return res
+    }
 }
 
-const swapi=new SwapiServise();
-swapi.getResource('/people/1/').then((p)=>console.log(p.name));
