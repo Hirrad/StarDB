25178682564234477bb02c689f9ce9cb1411f099
Klim Klimentov <klimentov@i.ua>
2021-03-01 13:24:44 +0200

    Добавил Switch для отображения страницы 404

--- src/app/app.js
+++ src/app/app.js
@@ -5,9 +5,9 @@ import RandomPlanet from "../random-planet";
 import SwapiServiseContext from "../context";
 import SwapiServise from "../services/swapi";
 import {PeoplePages, PlanetsPages, StarhipsPage} from "../pages";
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import {RenderStarshipItem} from "../components-help";
-
+import ErrorMessage from "../error-message";
 export default class App extends Component {
     state = {
         dataServise: new SwapiServise()
@@ -21,6 +21,7 @@ export default class App extends Component {
                 <BrowserRouter>
                     <Header/>
                     <RandomPlanet/>
+                    <Switch>
                     <Route path='/' render={() => <h2>Welcom to SW!</h2>} exact/>
                     <Route path='/starships' component={StarhipsPage} exact/>
 
@@ -31,9 +32,11 @@ export default class App extends Component {
                        return <RenderStarshipItem id={match.params.id}/>
                     }
                     }/>
+                    <Route component={ErrorMessage}/>
                     {/*<StarhipsPage/>*/}
                     {/*<PlanetsPages/>*/}
                     {/*<PeoplePages/>*/}
+                    </Switch>
                 </BrowserRouter>
             </SwapiServiseContext.Provider>
         </div>;
